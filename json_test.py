0c30406478810b115112bab66806ddf40aba0174
Toro0510 <234067517@qq.com>
2021-01-16 21:04:01 +0800

    更新部分代码

--- json_test.py
+++ json_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-df = pd.read_excel(r'C:\Users\jizeyuan\Desktop\短信分词\新建文件夹\短信JSON.xlsx', sheet_name='Sheet1')
+df = pd.read_excel(r'短信JSON.xlsx', sheet_name='Sheet1')
 
 dict_df = {}
 dict_df['order_number'] = str(df['order_number'][0])
@@ -9,7 +9,7 @@ dict_df['sms'] = list( df['body'])
 
 json_str = json.dumps(dict_df, ensure_ascii=False)
 
-with open("json_str.txt", "w", encoding='utf-8') as f:
+with open("test_str.txt", "w", encoding='utf-8') as f:
     f.write(json_str)
 
 # print(df.to_json(orient='index',force_ascii=False))
